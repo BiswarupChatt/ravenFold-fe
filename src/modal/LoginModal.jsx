c1e4eacca4912ac39c1de1e8b3fcb2323334b912
R Q <chatterjeebiswarup61@gmail.com>
2026-08-09 09:45:53 +0530

    changes

--- src/modal/LoginModal.jsx
+++ src/modal/LoginModal.jsx
@@ -1,8 +1,6 @@
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import GoogleIcon from '@mui/icons-material/Google'
 import {
   Alert,
   Box,
@@ -180,6 +178,82 @@ function AuthOptionButton({
       </Box>
       {loading ? loadingText : children}
     </ButtonBase>
+  )
+}
+
+function CompactProviderButton({ ariaLabel, disabled, icon, loading, onClick }) {
+  return (
+    <IconButton
+      aria-label={ariaLabel}
+      disabled={disabled || loading}
+      onClick={onClick}
+      sx={{
+        border: '1px solid',
+        borderColor: 'divider',
+        borderRadius: '50%',
+        color: 'text.primary',
+        height: 42,
+        width: 42,
+        '&:hover': {
+          bgcolor: 'rgba(30, 41, 82, 0.04)',
+          borderColor: 'primary.light',
+        },
+      }}
+    >
+      {loading ? (
+        <CircularProgress color="inherit" size={20} thickness={5} />
+      ) : (
+        icon
+      )}
+    </IconButton>
+  )
+}
+
+function GoogleBrandIcon() {
+  return (
+    <Box
+      aria-hidden="true"
+      component="svg"
+      viewBox="0 0 24 24"
+      sx={{ height: 28, width: 28 }}
+    >
+      <path
+        d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.58c2.08-1.92 3.27-4.74 3.27-8.09z"
+        fill="#4285F4"
+      />
+      <path
+        d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.58-2.77c-.98.66-2.23 1.06-3.7 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+        fill="#34A853"
+      />
+      <path
+        d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z"
+        fill="#FBBC05"
+      />
+      <path
+        d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+        fill="#EA4335"
+      />
+    </Box>
+  )
+}
+
+function FacebookBrandIcon() {
+  return (
+    <Box
+      aria-hidden="true"
+      component="svg"
+      viewBox="0 0 24 24"
+      sx={{ height: 28, width: 28 }}
+    >
+      <path
+        d="M24 12.07C24 5.41 18.63 0 12 0S0 5.41 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.03 1.79-4.7 4.53-4.7 1.31 0 2.68.24 2.68.24v2.96h-1.51c-1.49 0-1.96.93-1.96 1.89v2.27h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07z"
+        fill="#1877F2"
+      />
+      <path
+        d="M16.67 15.56l.53-3.49h-3.33V9.8c0-.96.47-1.89 1.96-1.89h1.51V4.95s-1.37-.24-2.68-.24c-2.74 0-4.53 1.67-4.53 4.7v2.66H7.08v3.49h3.04V24a12.2 12.2 0 0 0 3.75 0v-8.44h2.8z"
+        fill="#FFFFFF"
+      />
+    </Box>
   )
 }
 
@@ -505,7 +579,7 @@ function LoginModal({ open, onClose, onLoginSuccess }) {
     <Stack spacing={1.1}>
       <AuthOptionButton
         disabled={isSubmitting || providerLoading === 'facebook'}
-        icon={<GoogleIcon sx={{ color: '#4285f4' }} />}
+        icon={<GoogleBrandIcon />}
         loading={providerLoading === 'google'}
         onClick={handleGoogleLogin}
       >
@@ -514,7 +588,7 @@ function LoginModal({ open, onClose, onLoginSuccess }) {
 
       <AuthOptionButton
         disabled={isSubmitting || providerLoading === 'google'}
-        icon={<FacebookIcon sx={{ color: '#1877f2' }} />}
+        icon={<FacebookBrandIcon />}
         loading={providerLoading === 'facebook'}
         onClick={handleFacebookLogin}
       >
@@ -545,6 +619,35 @@ function LoginModal({ open, onClose, onLoginSuccess }) {
 
       <PolicyNotice onNavigate={handleClose} />
     </Stack>
+  )
+
+  const renderCompactProviderOptions = () => (
+    <Box
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        gap: 1.75,
+        justifyContent: 'center',
+        mx: 'auto',
+        width: '100%',
+      }}
+    >
+      <CompactProviderButton
+        ariaLabel="Continue with Google"
+        disabled={isSubmitting || providerLoading === 'facebook'}
+        icon={<GoogleBrandIcon />}
+        loading={providerLoading === 'google'}
+        onClick={handleGoogleLogin}
+      />
+
+      <CompactProviderButton
+        ariaLabel="Continue with Facebook"
+        disabled={isSubmitting || providerLoading === 'google'}
+        icon={<FacebookBrandIcon />}
+        loading={providerLoading === 'facebook'}
+        onClick={handleFacebookLogin}
+      />
+    </Box>
   )
 
   const renderEmailStep = () => (
@@ -651,7 +754,7 @@ function LoginModal({ open, onClose, onLoginSuccess }) {
         or continue with
       </Divider>
 
-      {renderProviderOptions()}
+      {renderCompactProviderOptions()}
 
       <PolicyNotice onNavigate={handleClose} />
     </Stack>
