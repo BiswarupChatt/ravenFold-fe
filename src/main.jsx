cbc96c6dfcc03f344ceb563367662785ab73c296
R Q <chatterjeebiswarup61@gmail.com>
2026-08-23 00:05:52 +0530

    uplaod fi

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import store from './store/store.js'
@@ -11,12 +11,12 @@ import theme from './theme.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
